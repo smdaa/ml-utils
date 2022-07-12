6c784845ea68fa51a0bad70ade0dddb348b118f1
SAAD MDAA <63920272+smdaa@users.noreply.github.com>
2022-07-12 19:21:46 +0200

    Update tf.py

--- tf.py
+++ tf.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from sklearn.model_selection import RepeatedKFold
+import tensorflow_model_optimization as tfmot
+import os
+import tempfile
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 def create_model():
     model = tf.keras.models.Sequential()
@@ -61,7 +66,7 @@ for train_index, test_index in kf.split(x):
     model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
 
     # train model
-    epochs     = 10
+    epochs     = 5
     batch_size = 32
     history    = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
     histories.append(history)
@@ -70,6 +75,43 @@ for train_index, test_index in kf.split(x):
     _, accuracy = model.evaluate(x_test, y_test, verbose=0)
     print('Test accuracy : %.3f' % (accuracy * 100.0))
     scores.append(accuracy)
+
+    model.save_weights('./model.h5')
+
+    # prune model
+    prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+    p_batch_size = 32
+    p_epochs = 2
+    p_validation_split = 0.1 
+
+    n_train = x_train.shape[0] * (1 - p_validation_split)
+    end_step = np.ceil(n_train / p_batch_size).astype(np.int32) * p_epochs
+
+    pruning_params = {
+      'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                               final_sparsity=0.80,
+                                                               begin_step=0,
+                                                               end_step=end_step)
+    }
+
+    model_for_pruning = prune_low_magnitude(model, **pruning_params)
+    model_for_pruning.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+
+    logdir = tempfile.mkdtemp()
+
+    callbacks = [
+        tfmot.sparsity.keras.UpdatePruningStep(),
+        tfmot.sparsity.keras.PruningSummaries(log_dir=logdir),
+    ]
+
+    model_for_pruning.fit(x_train, y_train,
+                  batch_size=p_batch_size, epochs=p_epochs, validation_split=p_validation_split,
+                  callbacks=callbacks)
+
+    model_for_export = tfmot.sparsity.keras.strip_pruning(model_for_pruning)
+    model_for_export.save_weights('./model_prun.h5')
+
+    exit()
 
 end = time.time()
 summarize_diagnostics(histories)
